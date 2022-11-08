dcd9dc462bed68f5851c1bd8e8a99bc7a17091e5
AsifKarim7 <asifkarim275@gmail.com>
2022-11-08 19:29:11 +0600

    auth context,login and signup done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
-    return (
-        <div>
 
+    const [error, setError] = useState('');
+    const { login } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message)
+            });
+    }
+
+    return (
+        <div className="hero w-full my-16">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src='https://img.freepik.com/free-vector/secure-login-concept-illustration_114360-4582.jpg?w=2000' alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-5xl text-center font-bold">Login</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="Enter Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="Enter Password" className="input input-bordered" />
+                        </div>
+                        <div>
+                            <p className=''>{error}</p>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center'>New to Moments Made? Please <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Signup = () => {
-    return (
-        <div>
 
+    const [error, setError] = useState('');
+    const { createUser } = useContext(AuthContext);
+
+    const handleSignup = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message)
+            });
+    }
+
+    return (
+        <div className="hero w-full my-16">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4' src='https://img.freepik.com/free-vector/secure-login-concept-illustration_114360-4582.jpg?w=2000' alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+                    <form onSubmit={handleSignup} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Enter Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name='photoURL' placeholder="Enter Photo URL" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="Enter Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="Enter Password" className="input input-bordered" required />
+
+                        </div>
+                        <div>
+                            <p className=''>{error}</p>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
+                        </div>
+                    </form>
+                    <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Login</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
