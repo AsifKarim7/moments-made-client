da0dbdfbaeabda4b50e824a185c869d793861049
AsifKarim7 <asifkarim275@gmail.com>
2022-11-09 01:24:14 +0600

    slider added in home page

--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -4,9 +4,14 @@ import 'react-awesome-slider/dist/styles.css';
 
 const Slider = () => {
     return (
-        <div>
-
-        </div>
+        <AwesomeSlider className='my-16'>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/DSCF7905.jpg" alt="1" /></div>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/DSCF5199-copy.jpg" alt="2" /></div>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/1.jpg" alt="3" /></div>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/www.weddingdiary.com_.bd0025-2.jpg" alt="4" /></div>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/www.weddingdiary.com_.bd0034.jpg" alt="5" /></div>
+            <div><img src="https://weddingdiary.com.bd/wp-content/uploads/2022/06/DSCF5428.jpg" alt="6" /></div>
+        </AwesomeSlider>
     );
 };
 
